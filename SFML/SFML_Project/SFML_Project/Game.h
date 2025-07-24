edb696989271b7eb019c52e4e4b09c57c1a61e1e
gydud0819 <pstar0819@naver.com>
2025-07-24 23:56:14 +0900

    (2025-07-24) UI 헤더 추가

--- SFML/SFML_Project/SFML_Project/Game.h
+++ SFML/SFML_Project/SFML_Project/Game.h
@@ -1,5 +1,11 @@
 #pragma once
 #include "Util.h"
+#include "Player.h"
+#include "StageManager.h"
+#include "SceneManager.h"
+#include "UIManager.h"
+#include "WaveManager.h"
+
 // 효 추가 : 게임의 전체적인 흐름 돌리기(필요없을수도 있음 보류)
 class Game
 {
